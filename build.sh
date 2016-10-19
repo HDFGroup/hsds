87be0ca0f9531756b2dcaafd8db63c4353357cd0
jreadey <jreadey@hdfgroup.org>
2016-10-18 20:55:51 -0700

    add no pyflakes option to build script

--- build.sh
+++ build.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-
-echo "running pyflakes on hsds"
-if [ $(./pyflakes.sh -count hsds) -ge 1 ]; then
-   echo "pyflakes errors in hsds..."
-   ./pyflakes.sh hsds
-   exit 1
+run_pyflakes=1
+if [ $# -gt 0 ]; then
+    if [ $1 == "-h" ] || [ $1 == "--help" ]; then
+        echo "Usage: build.sh [--nolint]"
+        exit 1
+    fi
+    if [ $1 == "--nolint" ]; then
+        echo "no pyflakes"
+        run_pyflakes=
+    fi
+fi 
+  
+if [ $run_pyflakes ]; then
+    echo "running pyflakes on hsds"
+    if [ $(./pyflakes.sh -count hsds) -ge 1 ]; then
+        echo "pyflakes errors in hsds..."
+        ./pyflakes.sh hsds
+        exit 1
+    fi
 fi
+
 echo "running pyflakes on hsds/util"
 if [ $(./pyflakes.sh -count hsds/util) -ge 1 ]; then
-   echo "pyflakes errors in hsds/util..."
-   ./pyflakes.sh hsds/util
-   exit 1
+    echo "pyflakes errors in hsds/util..."
+    ./pyflakes.sh hsds/util
+    exit 1
 fi
 
 echo "clean stopped containers"
