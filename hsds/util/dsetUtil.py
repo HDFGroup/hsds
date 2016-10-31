30e7f7a6c7f036530458627be64361497b4af35f
jreadey <jreadey@hdfgroup.org>
2016-10-31 10:26:55 -0700

    added util classes for chunk/dset operations

--- hsds/util/dsetUtil.py
+++ hsds/util/dsetUtil.py
@@ -1,4 +1,6 @@
 import numpy as np
+from aiohttp.errors import HttpBadRequest 
+import hsds_logger as log
 
 CHUNK_BASE = 16*1024    # Multiplier by which chunks are adjusted
 CHUNK_MIN = 8*1024      # Soft lower limit (8k)
@@ -56,3 +58,210 @@ def guess_chunk(shape, maxshape, typesize):
         idx += 1
 
     return tuple(int(x) for x in chunks)
+
+def getNumChunks(selection, layout):
+    """
+    Get the number of chunks potentially required.
+    If selection is provided (a list of slices), return the number
+    of chunks that intersect with the selection.
+    """
+    # do a quick check that we don't have a null selection space'
+    # TBD: this needs to be revise to do the right think with stride > 1
+    for s in selection:
+        if s.stop <= s.start:
+            log.info("null selection")
+            return 0
+    num_chunks = 1
+    for i in range(len(selection)): 
+        s = selection[i]
+        w = s.stop - s.start # selection width (>0)
+        c = layout[i]   # chunk size
+        partial_left = (c - (s.start % c)) % c
+        remainder = w - partial_left
+        if remainder == 0:
+            # if raminder is 0, then we just cross one chunk along this dimensions
+            continue
+        partial_right = remainder % c
+        count = (remainder - partial_right) // c
+        if partial_right > 0:
+            count += 1
+        if partial_left > 0:
+            count += 1
+        num_chunks *= count
+    return num_chunks
+            
+            
+def getChunkIds(dset_id, selection, layout, dim=0, prefix=None, chunk_ids=None):
+    num_chunks = getNumChunks(selection, layout)
+    if num_chunks == 0:
+        return []  # empty list
+    if prefix is None:
+        # construct a prefix using "c-" with the uuid of the dset_id
+        if not dset_id.startswith("d-"):
+            msg = "Bad Request: invalid dset id: {}".format(dset_id)
+            log.warn(msg)
+            raise HttpBadRequest(message=msg)
+        prefix = "c-" + dset_id[2:] + '_'
+    rank = len(selection)
+    if chunk_ids is None:
+        chunk_ids = []
+    s = selection[dim]
+    c = layout[dim]
+    chunk_index_start = s.start // c
+    chunk_index_end = s.stop // c
+    if s.stop % c != 0:
+        chunk_index_end += 1
+    for i in range(chunk_index_start, chunk_index_end):
+        chunk_id = prefix + str(i)
+        if dim + 1 == rank:
+            # we've gone through all the dimensions, add this id to the list
+            chunk_ids.append(chunk_id)
+        else:
+            chunk_id += '_'  # seperator between dimensions
+            # recursive call
+            getChunkIds(dset_id, selection, layout, dim+1, chunk_id, chunk_ids)
+    # got the complete list, return it!
+    return chunk_ids
+
+    
+def getChunkIndex(chunk_id):
+    """ given a chunk_id (e.g.: c-12345678-1234-1234-1234-1234567890ab_6_4) 
+    return the coordinates of the chunk. In this case (6,4)
+    """  
+    # go to the first underscal
+    n = chunk_id.find('_')  + 1
+    if n == 0:
+        raise ValueError("Invalid chunk_id: {}".format(chunk_id))
+    suffix = chunk_id[n:]   
+    
+    index = []
+    parts = suffix.split('_')
+    for part in parts:
+        index.append(int(part))
+
+    return index
+    
+
+def getHyperslabSelection(dsetshape, start=None, stop=None, step=None):
+    """
+    Get slices given lists of start, stop, step values
+
+    TBD: for step>1, adjust the slice to not extend beyond last data point returned
+    """
+    rank = len(dsetshape)
+    if start:
+        if not isinstance(start, (list, tuple)):
+            start = [start]
+        if len(start) != rank:
+            msg = "Bad Request: start array length not equal to dataset rank"
+            log.warn(msg)
+            raise HttpBadRequest(message=msg)
+        for dim in range(rank):
+            if start[dim] < 0 or start[dim] >= dsetshape[dim]:
+                msg = "Bad Request: start index invalid for dim: " + str(dim)
+                log.warn(msg)
+                raise HttpBadRequest(message=msg)
+    else:
+        start = []
+        for dim in range(rank):
+            start.append(0)
+    
+    if stop:
+        if not isinstance(stop, (list, tuple)):
+            stop = [stop]
+        if len(stop) != rank:
+            msg = "Bad Request: stop array length not equal to dataset rank"
+            log.warn(msg)
+            raise HttpBadRequest(message=msg)
+        for dim in range(rank):
+            if stop[dim] <= start[dim] or stop[dim] > dsetshape[dim]:
+                msg = "Bad Request: stop index invalid for dim: " + str(dim)
+                log.warn(msg)
+                raise HttpBadRequest(message=msg)
+    else:
+        stop = []
+        for dim in range(rank):
+            stop.append(dsetshape[dim])
+
+    if step:
+        if not isinstance(step, (list, tuple)):
+            step = [step]
+        if len(step) != rank:
+            msg = "Bad Request: step array length not equal to dataset rank"
+            log.warn(msg)
+            raise HttpBadRequest(message=msg)
+        for dim in range(rank):
+            if step[dim] <= 0 or step[dim] > dsetshape[dim]:
+                msg = "Bad Request: step index invalid for dim: " + str(dim)
+                log.warn(msg)
+                raise HttpBadRequest(message=msg)
+    else:
+        step = []
+        for dim in range(rank):
+            step.append(1)
+
+    slices = []
+    
+    for dim in range(rank):
+        
+        try:
+            s = slice(int(start[dim]), int(stop[dim]), int(step[dim]))
+        except ValueError:
+            msg = "Bad Request: invalid start/stop/step value"
+            log.warn(msg)
+            raise HttpBadRequest(message=msg)
+        slices.append(s)
+    return tuple(slices)
+
+
+def getChunkSelection(chunk_id, slices, layout):
+    """ 
+    Return the intersection of the chunk with the given slices selection of the array.
+    """
+    chunk_index = getChunkIndex(chunk_id)
+    rank = len(layout)
+    sel = []
+    for dim in range(rank):
+        s = slices[dim]
+        w = layout[dim]
+        n = chunk_index[dim] * w 
+        if s.start < n:
+            start = n
+        else:
+            start = s.start
+        if s.start >= n + w:
+            return None  # null intersection
+        if s.stop >= n + w:
+            stop = n + w
+        else:
+            stop = s.stop
+
+        step = 1 # TBD - deal with steps > 1
+        sel.append(slice(start, stop, step))
+    return sel
+
+def getChunkCoverage(chunk_id, slices, layout):
+    """
+    Get chunk-relative selection of the given chunk and selection.
+    """
+    chunk_index = getChunkIndex(chunk_id)
+    rank = len(layout)
+    sel = []
+    for dim in range(rank):
+        s = slices[dim]
+        w = layout[dim]
+        offset = chunk_index[dim] * w
+        start = s.start - offset
+        if start < 0:
+            msg = "Unexpected chunk selection"
+            log.error(msg)
+            raise ValueError(msg)
+        stop = s.stop - offset
+        if stop > w:
+            msg = "Unexpected chunk selection"
+            log.error(msg)
+            raise ValueError(msg)
+        step = 1  # TBD - update for step > 1
+        sel.append(slice(start, stop, step))
+    return sel
+
--- tests/unit/dsetUtilTest.py
+++ tests/unit/dsetUtilTest.py
@@ -14,7 +14,9 @@ import sys
  
 sys.path.append('../../hsds/util')
 sys.path.append('../../hsds')
-from dsetUtil import guess_chunk
+from dsetUtil import guess_chunk, getHyperslabSelection, getNumChunks
+from dsetUtil import getChunkIds, getChunkIndex, getChunkSelection, getChunkCoverage
+
 
 class DsetUtilTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
@@ -38,7 +40,269 @@ class DsetUtilTest(unittest.TestCase):
 
         shape = [100, 0]
         layout = guess_chunk(shape, None, typesize)
-        print(layout)
+
+    def testGetNumChunks(self):
+        datashape = [100,]
+        layout = (10,)
+        selection = getHyperslabSelection(datashape)
+        count = getNumChunks(selection, layout)
+        self.assertEqual(count, 10)
+        selection = getHyperslabSelection(datashape, 12, 83)
+        count = getNumChunks(selection, layout)
+        self.assertEqual(count, 8)
+        selection = getHyperslabSelection(datashape, 12, 80)
+        count = getNumChunks(selection, layout)
+        self.assertEqual(count, 7)
+        selection = getHyperslabSelection(datashape, 10, 83)
+        count = getNumChunks(selection, layout)
+        self.assertEqual(count, 8)
+        selection = getHyperslabSelection(datashape, 12, 17)
+        count = getNumChunks(selection, layout)
+        self.assertEqual(count, 1)
+
+        datashape = [100,100]
+        layout = (10,5)
+        selection = getHyperslabSelection(datashape)
+        count = getNumChunks(selection, layout)
+        self.assertEqual(count, 200)
+        selection = getHyperslabSelection(datashape, (41, 6), (49, 9))
+        count = getNumChunks(selection, layout)
+        self.assertEqual(count, 1)
+        selection = getHyperslabSelection(datashape, (39, 4), (47, 7))
+        count = getNumChunks(selection, layout)
+        self.assertEqual(count, 4)
+
+
+
+    def testGetHyperslabSelection(self):
+        # getHyperslabSelection(dsetshape, start, stop, step)
+        # 1-D case
+        datashape = [100,]
+        slices = getHyperslabSelection(datashape)
+        self.assertEqual(len(slices), 1)
+        self.assertEqual(slices[0], slice(0, 100, 1))
+
+        slices = getHyperslabSelection(datashape, 20)
+        self.assertEqual(len(slices), 1)
+        self.assertEqual(slices[0], slice(20, 100, 1))
+
+        slices = getHyperslabSelection(datashape, 20, 80)
+        self.assertEqual(len(slices), 1)
+        self.assertEqual(slices[0], slice(20, 80, 1))
+
+        slices = getHyperslabSelection(datashape, 20, 80, 2)
+        self.assertEqual(len(slices), 1)
+        self.assertEqual(slices[0], slice(20, 80, 2))
+
+        datashape = [100, 50]
+        slices = getHyperslabSelection(datashape)
+        self.assertEqual(len(slices), 2)
+        self.assertEqual(slices[0], slice(0, 100, 1))
+        self.assertEqual(slices[1], slice(0, 50, 1))
+
+        slices = getHyperslabSelection(datashape, (10, 20))
+        self.assertEqual(len(slices), 2)
+        self.assertEqual(slices[0], slice(10, 100, 1))
+        self.assertEqual(slices[1], slice(20, 50, 1))
+
+        slices = getHyperslabSelection(datashape, (10, 20), (90, 30))
+        self.assertEqual(len(slices), 2)
+        self.assertEqual(slices[0], slice(10, 90, 1))
+        self.assertEqual(slices[1], slice(20, 30, 1))
+
+        slices = getHyperslabSelection(datashape, (10, 20), (90, 30), (1,2))
+        self.assertEqual(len(slices), 2)
+        self.assertEqual(slices[0], slice(10, 90, 1))
+        self.assertEqual(slices[1], slice(20, 30, 2))
+    
+    def testGetChunkIds(self):
+        # getChunkIds(dset_id, selection, layout, dim=0, prefix=None, chunk_ids=None):
+        dset_id = "d-12345678-1234-1234-1234-1234567890ab"
+        datashape = [100,]
+        layout = (10,)
+        selection = getHyperslabSelection(datashape)
+        chunk_ids = getChunkIds(dset_id, selection, layout)
+        self.assertEqual(len(chunk_ids), 10)
+        for i in range(10):
+            chunk_id = chunk_ids[i]
+            # chunk_id should look like:
+            # c-12345678-1234-1234-1234-1234567890ab_n
+            # where 'n' is in the range 0-9
+            self.assertTrue(chunk_id.startswith("c-"))
+            self.assertTrue(chunk_id.endswith('_' + str(i)))
+            self.assertEqual(chunk_id[2:-2], dset_id[2:])
+            self.assertEqual(len(chunk_id), 2+36+2)
+
+        selection = getHyperslabSelection(datashape, 20)
+        chunk_ids = getChunkIds(dset_id, selection, layout)
+        self.assertEqual(len(chunk_ids), 8)
+        for i in range(8):
+            chunk_id = chunk_ids[i]
+            self.assertTrue(chunk_id.startswith("c-"))
+            self.assertTrue(chunk_id.endswith('_' + str(i+2)))
+            self.assertEqual(chunk_id[2:-2], dset_id[2:])
+            self.assertEqual(len(chunk_id), 2+36+2)
+
+        selection = getHyperslabSelection(datashape, 20, 81)
+        chunk_ids = getChunkIds(dset_id, selection, layout)
+        self.assertEqual(len(chunk_ids), 7)
+        for i in range(7):
+            chunk_id = chunk_ids[i]
+            self.assertTrue(chunk_id.startswith("c-"))
+            self.assertTrue(chunk_id.endswith('_' + str(i+2)))
+            self.assertEqual(chunk_id[2:-2], dset_id[2:])
+            self.assertEqual(len(chunk_id), 2+36+2)
+
+        selection = getHyperslabSelection(datashape, 29, 81)
+        chunk_ids = getChunkIds(dset_id, selection, layout)
+        self.assertEqual(len(chunk_ids), 7)
+        for i in range(7):
+            chunk_id = chunk_ids[i]
+            self.assertTrue(chunk_id.startswith("c-"))
+            self.assertTrue(chunk_id.endswith('_' + str(i+2)))
+            self.assertEqual(chunk_id[2:-2], dset_id[2:])
+            self.assertEqual(len(chunk_id), 2+36+2)
+
+        datashape = [100,100]
+        layout = (10,20)
+        selection = getHyperslabSelection(datashape)
+        chunk_ids = getChunkIds(dset_id, selection, layout)
+        self.assertEqual(len(chunk_ids), 50)
+        chunk_ids.reverse() # so we can pop off the front
+        for i in range(10):
+            for j in range(5):
+                chunk_id = chunk_ids.pop()
+                self.assertTrue(chunk_id.startswith("c-"))
+                index1 = int(chunk_id[-3])
+                index2 = int(chunk_id[-1])
+                self.assertEqual(index1, i)
+                self.assertEqual(index2, j)
+
+    def testGetChunkIndex(self):
+        chunk_id = "c-12345678-1234-1234-1234-1234567890ab_6_4"
+        index = getChunkIndex(chunk_id)
+        self.assertEqual(index, [6,4])
+        chunk_id = "c-12345678-1234-1234-1234-1234567890ab_64"
+        index = getChunkIndex(chunk_id)
+        self.assertEqual(index, [64,])
+
+
+    def testGetChunkSelection(self):
+
+        # 1-d test
+        dset_id = "d-12345678-1234-1234-1234-1234567890ab"
+        datashape = [100,]
+        layout = (10,)
+        selection = getHyperslabSelection(datashape, 42, 62)
+        chunk_ids = getChunkIds(dset_id, selection, layout)
+        self.assertEqual(len(chunk_ids), 3)
+
+        chunk_id = chunk_ids[0]
+        sel = getChunkSelection(chunk_id, selection, layout)
+        self.assertEqual(sel[0].start, 42)
+        self.assertEqual(sel[0].stop, 50)
+        self.assertEqual(sel[0].step, 1)
+        cc = getChunkCoverage(chunk_id, sel, layout)
+        self.assertEqual(cc[0].start, 2)
+        self.assertEqual(cc[0].stop, 10)
+        self.assertEqual(cc[0].step, 1)
+
+        chunk_id = chunk_ids[1]
+        sel = getChunkSelection(chunk_id, selection, layout)
+        self.assertEqual(sel[0].start, 50)
+        self.assertEqual(sel[0].stop, 60)
+        self.assertEqual(sel[0].step, 1)
+        cc = getChunkCoverage(chunk_id, sel, layout)
+        self.assertEqual(cc[0].start, 0)
+        self.assertEqual(cc[0].stop, 10)
+        self.assertEqual(cc[0].step, 1)
+
+        chunk_id = chunk_ids[2]
+        sel = getChunkSelection(chunk_id, selection, layout)
+        self.assertEqual(sel[0].start, 60)
+        self.assertEqual(sel[0].stop, 62)
+        self.assertEqual(sel[0].step, 1)
+        cc = getChunkCoverage(chunk_id, sel, layout)
+        self.assertEqual(cc[0].start, 0)
+        self.assertEqual(cc[0].stop, 2)
+        self.assertEqual(cc[0].step, 1)
+
+
+        # 2-d test
+        dset_id = "d-12345678-1234-1234-1234-1234567890ab"
+        datashape = [100,100]
+        layout = (10,10)
+        selection = getHyperslabSelection(datashape, (42, 46), (52, 58))
+        chunk_ids = getChunkIds(dset_id, selection, layout)
+        self.assertEqual(len(chunk_ids), 4)
+
+        chunk_id = chunk_ids[0]
+        sel = getChunkSelection(chunk_id, selection, layout)
+        self.assertEqual(sel[0].start, 42)
+        self.assertEqual(sel[0].stop, 50)
+        self.assertEqual(sel[0].step, 1)
+        self.assertEqual(sel[1].start, 46)
+        self.assertEqual(sel[1].stop, 50)
+        self.assertEqual(sel[1].step, 1)
+
+        chunk_id = chunk_ids[1]
+        print(chunk_id)
+        sel = getChunkSelection(chunk_id, selection, layout)
+        self.assertEqual(sel[0].start, 42)
+        self.assertEqual(sel[0].stop, 50)
+        self.assertEqual(sel[0].step, 1)
+        self.assertEqual(sel[1].start, 50)
+        self.assertEqual(sel[1].stop, 58)
+        self.assertEqual(sel[1].step, 1) 
+
+        chunk_id = chunk_ids[2]
+        print(chunk_id)
+        sel = getChunkSelection(chunk_id, selection, layout)
+        self.assertEqual(sel[0].start, 50)
+        self.assertEqual(sel[0].stop, 52)
+        self.assertEqual(sel[0].step, 1)
+        self.assertEqual(sel[1].start, 46)
+        self.assertEqual(sel[1].stop, 50)
+        self.assertEqual(sel[1].step, 1)
+
+        
+
+        chunk_id = chunk_ids[3]
+        print(chunk_id)
+        sel = getChunkSelection(chunk_id, selection, layout)
+        self.assertEqual(sel[0].start, 50)
+        self.assertEqual(sel[0].stop, 52)
+        self.assertEqual(sel[0].step, 1)
+        self.assertEqual(sel[1].start, 50)
+        self.assertEqual(sel[1].stop, 58)
+        self.assertEqual(sel[1].step, 1)
+
+        # 1-d test with fractional chunks
+        datashape = [104,]
+        layout = (10,)
+        selection = getHyperslabSelection(datashape, 92, 102)
+        chunk_ids = getChunkIds(dset_id, selection, layout)
+        self.assertEqual(len(chunk_ids), 2)
+
+        chunk_id = chunk_ids[0]
+        print(chunk_id)
+        sel = getChunkSelection(chunk_id, selection, layout)
+        sel = sel[0]
+        self.assertEqual(sel.start, 92)
+        self.assertEqual(sel.stop, 100)
+        self.assertEqual(sel.step, 1)
+
+        chunk_id = chunk_ids[1]
+        print(chunk_id)
+        sel = getChunkSelection(chunk_id, selection, layout)
+        sel = sel[0]
+        self.assertEqual(sel.start, 100)
+        self.assertEqual(sel.stop, 102)
+        self.assertEqual(sel.step, 1)
+
+             
+
+        
 
                                   
              
