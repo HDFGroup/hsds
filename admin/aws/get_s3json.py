714a9ff5b62ba31e8f612c118b907c1c2776decd
jreadey <jreadey@hdfgroup.org>
2016-12-15 17:32:33 -0800

    fancy printing for aws util

--- admin/aws/get_s3json.py
+++ admin/aws/get_s3json.py
@@ -11,6 +11,8 @@
 ##############################################################################
 import sys
 import json
+import time
+import hashlib
 import boto.s3
 import boto.s3.connection 
 from boto.s3.key import Key
@@ -21,6 +23,7 @@ import config
     
 # NOTE - this script does not seem to work with minio - see: https://github.com/mozilla/socorro-collector/issues/17
 #
+
 # Print usage and exit
 #
 def printUsage():
@@ -28,7 +31,17 @@ def printUsage():
     print("  objid: s3 JSON obj to fetch")
     print("  Example: python get_s3json --aws_s3_gateway=http://192.168.99.100:9000 --bucket_name=minio.hsdsdev t-cf2fc310-996f-11e6-8ef6-0242ac110005")
     sys.exit(); 
-       
+
+#
+# Get hash prefix
+#
+def getIdHash(id):
+    """  Return md5 prefix based on id value"""
+    m = hashlib.new('md5')
+    m.update(id.encode('utf8'))
+    hexdigest = m.hexdigest()
+    return hexdigest[:5]
+
                
 def main():
     if len(sys.argv) == 1 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
@@ -37,19 +50,23 @@ def main():
     
     obj_id = sys.argv[-1]
     s3_gateway = config.get("aws_s3_gateway")
-    print("aws_s3_gateway:", s3_gateway)
+    print("aws_s3_gateway: {}".format(s3_gateway))
     region = config.get("aws_region")
-    print("region:", region)
+    print("region: {}".format(region))
+    print("now: {}".format(time.time()))
     conn = boto.s3.connect_to_region(
         region,
-        #aws_access_key_id=awsAccessKeyID,
-        #aws_secret_access_key=awsSecretKey,
         calling_format=boto.s3.connection.OrdinaryCallingFormat()
     )
     
     bucket_name = config.get("bucket_name")
-    print("bucket_name:", bucket_name)
+    print("bucket_name: {}".format(bucket_name))
     bucket = conn.get_bucket(bucket_name)
+
+    if obj_id.startswith("d-") or obj_id.startswith("g-") or obj_id.startswith("t-"):
+        # add the checksum prefix
+        obj_id = getIdHash(obj_id) + '-' + obj_id
+
     k = Key(bucket)
     k.key = obj_id
     data = k.get_contents_as_string()
--- admin/aws/list_instances.py
+++ admin/aws/list_instances.py
@@ -1,10 +1,20 @@
 import boto.ec2
-conn = boto.ec2.connect_to_region("us-west-2")
+import config
+
+region = config.get("aws_region")
+conn = boto.ec2.connect_to_region(region)
 reservations = conn.get_all_instances()
+fields = ("id", "public ip", "private ip", "name", "state")
+format_str = "{:<12} {:<16} {:<16} {:<16} {:<12}"
+print(format_str.format(*fields))
+sep = ('----------',) * 5
+print(format_str.format(*sep))
 for res in reservations:
     for inst in res.instances:
+        name = '---'
         if 'Name' in inst.tags:
-            print "%s id: %s public: %s private: %s state: %s]" % (inst.tags['Name'], inst.id, inst.ip_address, inst.private_ip_address, inst.state)
-        else:
-            print "%s public: %s private: %s state: %s" % (inst.id, inst.ip_address, inst.private_ip_address, inst.state)
+            name = inst.tags["Name"]
+        print(format_str.format(inst.id, inst.ip_address, inst.private_ip_address, name, inst.state))
+
+         
 
