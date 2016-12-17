08b1553bf1a786b4bc58fbf342dc43a975a947f0
jreadey <jreadey@hdfgroup.org>
2016-12-17 13:17:18 -0800

    fix for running list_instances on PY3

--- admin/aws/list_instances.py
+++ admin/aws/list_instances.py
@@ -7,13 +7,15 @@ reservations = conn.get_all_instances()
 fields = ("id", "public ip", "private ip", "name", "state")
 format_str = "{:<20} {:<16} {:<16} {:<16} {:<12}"
 print(format_str.format(*fields))
-sep = ('----------',) * 5
+sep = ('-'*12,) * 5
 print(format_str.format(*sep))
 for res in reservations:
     for inst in res.instances:
-        name = '---'
+        name = '<none>'
         if 'Name' in inst.tags:
             name = inst.tags["Name"]
+        if inst.ip_address is None:
+            inst.ip_address = '<none>'
         print(format_str.format(inst.id, inst.ip_address, inst.private_ip_address, name, inst.state))
 
          
