3f2a16b4626858e4db10ff908f1e255931536c6d
Andrew Michaelis <amac@hyperplane.org>
2017-03-25 20:56:06 +0000

    add aws ecs hsds startup example

---
+++ admin/ecs/README.md
@@ -0,0 +1,63 @@
+HSDS - Using The Amazon EC2 Container Service
+==============================================
+ 
+This is a basic example illustrating how one might package and run 
+the hsds service using the amazon web services EC2 Container Service.
+
+Prologue
+--------
+
+For simplicity, you should first go to the EC2 Container Service on the aws 
+web console and then create a repository (ECR). Note, it is advised you create 
+an IAM group with the appropriate permissions and attached policies and then 
+add/create a user and attach it to the group. The permissions for this simple 
+example will also require S3 access to the bucket that the hsds service will 
+use, such as GET/PUT/delete/list, since hsds will read, write and find objects in S3.
+
+1. See [Amazon EC2 Container Service - Getting Started](http://docs.aws.amazon.com/AmazonECS/latest/developerguide/get-set-up-for-amazon-ecs.html)
+2. Create a repository, for simplicity call it _hsds_. Make sure your IAM user has the appropriate access permissions for the ECS service.
+3. Determine your ecr base URI, it looks something this **https://158023651469.dkr.ecr.us-west-2.amazonaws.com**
+4. Create an S3 bucket, for simplicity call it something like XXXXXhsds, replace XXXXX with some random characters of your choosing. Make sure 
+   your IAM user has the appropriate access permissions for the bucket XXXXXhsds. 
+
+Building the image, buildecr.sh  
+-------------------------------
+
+These steps and using the buildecr.sh script will build the docker image and push it to your newly created ecr repository hsds (done in prologue).
+
+1. Login to your build host (the host where you will build the docker image) if you're not already there.
+2. Get project source code: `$ git clone https://github.com/HDFGroup/hsds`
+3. Go to install directory: `$ cd hsds`
+4. Copy buildecr.sh example script to your current working directory: `$ cp admin/ecs/buildecr.sh .`
+5. Create an environment variable to pass the bucket name that was created in the Prologue: `$ export BUCKET_NAME=XXXXXhsds
+6. Open buildecr.sh in your favorite editor and set the REPURI shell variable to your ecr URI.
+7. Login to the ecr service via (note, you may need to change the region): `$ aws ecr get-login --region us-west-2`, which will print 
+   something like: **docker login -u AWS -p <big-b64-string> -e none https://158023651469.dkr.ecr.us-west-2.amazonaws.com**, cut and paste this 
+   to the shell to complete the login, e.g. `$ docker login -u AWS -p <big-b64-string> -e none https://158023651469.dkr.ecr.us-west-2.amazonaws.com`
+8. Run the buildecr.sh script: `$ buildecr.sh` 
+9. Optional, check your repository in aws web console and see that hsds image is indeed there.
+
+#### Notes
+* The build script will create a credential file in admin/config/passwd.txt, of which hsds will use for basic authentication (2017-03)
+
+Running the containers, runecs.sh (locally using the aws repository) 
+---------------------------------------------------------------------
+
+0. Make sure you are still logged in to the ecr service, if not use the aws ecr get-login as done above.
+1. Make sure you have a local docker daemon up and running. See the [install docker on aws if needed](http://docs.aws.amazon.com/AmazonECS/latest/developerguide/docker-basics.html#install_docker)
+2. Go to this example directory: `$ cd admin/ecs`
+3. Run the runecs.sh script: `$ ./runecs.sh --help` to see the which environ variables need to be set, then set 
+   them. Some of these are related to what was set in buildecr.sh script.
+4. Now run the runecs.sh script: `$ ./runecs.sh 2` 
+
+#### Notes
+
+* If you see the error 'docker: Error response from daemon: Conflict. The name "/hsds_head"' this suggests you may need to do some cleanup 
+  from a previous hsds instantiation. Once can do a cleanup via `$ docker ps -aq --filter name="hsds\*" |  xargs docker rm`
+
+* The runecs.sh script will attempt to create a root hsds domain via the create_toplevel_domain_json.py (in ../../hsds/create_toplevel_domain_json.py relative to this README).
+
+Running the containers, runecs.sh (using the aws repository and the aws container cluster) 
+-------------------------------------------------------------------------------------------
+
+
---
+++ admin/ecs/runecs.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+
+#
+# A simple run hsds example on the aws container service 
+# Note, this is as of Mar,24 2017
+#
+# You will need to login from the build host, e.g. 
+# $ aws ecr get-login --region us-west-2
+# which will print something like:
+# docker login -u AWS -p <big-b64-string> -e none https://158023651469.dkr.ecr.us-west-2.amazonaws.com
+#
+# Note, you will need to use docker version >= 1.9 for this.
+#
+
+HEAD_PORT=5100
+DN_PORT=5101
+SN_PORT=5102
+CONT_NETWORK=hsdsnet
+
+CNT=
+CLUST=
+HOSTIP=
+
+if [ -z "$1" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+   echo "runecs.sh <count> [cluster]"
+   echo ""
+   echo "This script expects the following environ variables to be set:"
+   echo "AWS_S3_GATEWAY, AWS_REGION, AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID, BUCKET_NAME, REPURI, REPNAME, HSDS_ADMIN, ROOTDOMAIN"
+   echo "== Example =="
+   echo "export AWS_S3_GATEWAY=https://s3-us-west-2.amazonaws.com" 
+   echo "export AWS_REGION=us-west-2"
+   echo "export AWS_SECRET_ACCESS_KEY=1E1IVEleao4omF3l3mCQ77vT6nfqfWfFBp/CVM56"
+   echo "export AWS_ACCESS_KEY_ID=BKIBIN4H4DG2B8RK23BB"
+   echo "export BUCKET_NAME=abcdehsds"
+   echo "export REPURI=158023651469.dkr.ecr.us-west-2.amazonaws.com"
+   echo "export REPNAME=hsds"
+   echo "export HSDS_ADMIN=adminuser"
+   echo "export ROOTDOMAIN=/data"
+   exit 1
+else
+   CNT=$1
+fi
+
+if [ ! -z "$2" ]; then
+   CLUST="yes"
+fi
+
+
+#---------------------------------------------------------------------------
+# Note, this depends on being able to reach the internet 
+function mylocalip {
+   # use ames internet root name server ns.nasa.gov for ip route test
+   HOSTIP=$(ip route get 192.203.230.10 | awk '/192.203.230.10/ {print $NF}')
+}
+
+#---------------------------------------------------------------------------
+# Makes a local container network so nodes can reach each other.
+# Note, the --link is now deprecated...
+function make_local_container_network {
+   isavail=`docker network ls --filter name=$CONT_NETWORK | wc -l`
+   if [ "$isavail" != "2" ]; then
+      echo "Initializing docker network..." 
+      docker network create --driver bridge $CONT_NETWORK
+   fi
+   echo "== Current docker networks =="
+   docker network ls
+   echo "== The $CONT_NETWORK docker network =="
+   docker network inspect $CONT_NETWORK
+}
+
+#---------------------------------------------------------------------------
+# Routine must be called like "run_head N", where N is a number fomr 1 to M
+function run_head {
+   count=$1
+   echo "starting the head on listening on port ${HEAD_PORT}"
+   docker run -d -p ${HEAD_PORT}:${HEAD_PORT} --name hsds_head \
+      --env TARGET_SN_COUNT=${count} \
+      --env TARGET_DN_COUNT=${count} \
+      --env HEAD_PORT=${HEAD_PORT} \
+      --env HEAD_HOST="hsds_head" \
+      --env NODE_TYPE="head_node"  \
+      --env AWS_S3_GATEWAY=${AWS_S3_GATEWAY} \
+      --env AWS_REGION=${AWS_REGION} \
+      --env AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+      --env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+      --env BUCKET_NAME=${BUCKET_NAME} \
+      --network=$CONT_NETWORK \
+        ${REPURI}/${REPNAME}:latest
+}
+
+#-------------------------------------------------------------------------------
+# Routine must be called like "run_datanode  N", where N is a number fomr 1 to M
+function run_datanode {
+   count=$1
+   for i in $(seq 1 $count); do    
+      NAME="hsds_dn_"$(($i-1))
+      docker run -d -p ${DN_PORT}:${DN_PORT} --name $NAME \
+         --env DN_PORT=${DN_PORT} \
+         --env NODE_TYPE="dn"  \
+         --env AWS_S3_GATEWAY=${AWS_S3_GATEWAY} \
+         --env AWS_REGION=${AWS_REGION} \
+         --env AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+         --env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+         --env BUCKET_NAME=${BUCKET_NAME} \
+         --network=$CONT_NETWORK \
+         ${REPURI}/${REPNAME}:latest
+      DN_PORT=$(($DN_PORT+2))
+   done
+}
+
+#-------------------------------------------------------------------------------
+# Routine must be called like "run_servicenode  N", where N is a number fomr 1 to M
+function run_servicenode  {
+   count=$1
+   for i in $(seq 1 $count); do    
+      NAME="hsds_sn_"$(($i-1))
+      docker run -d -p ${SN_PORT}:${SN_PORT} --name $NAME \
+         --env SN_PORT=${SN_PORT} \
+         --env NODE_TYPE="sn"  \
+         --env AWS_S3_GATEWAY=${AWS_S3_GATEWAY} \
+         --env AWS_REGION=${AWS_REGION} \
+         --env AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+         --env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+         --env BUCKET_NAME=${BUCKET_NAME} \
+         --network=$CONT_NETWORK \
+         ${REPURI}/${REPNAME}:latest
+      SN_PORT=$(($SN_PORT+2))
+   done    
+}
+
+#---------------------------------------------------------------------------
+# The hsds service requires a root domain to be specified before you
+# can add/manipulate any other domain; here by domain we're refering to
+# the hdf5 REST definition, not a network def'n.
+function init_h5_root_domain_local {
+   docker exec hsds_head \
+   python3 /usr/local/src/hsds/create_toplevel_domain_json.py --user=$HSDS_ADMIN  --domain=$ROOT_DOMAIN
+}
+
+if [ "$CLUST" == "yes" ]; then
+   echo "Running with ecs docker cluster..."
+   echo "ECS cluster example not implemented yet..."
+   exit 1
+else
+   echo "Running with local docker..."
+   make_local_container_network 
+   run_head $CNT
+   run_datanode $CNT
+   run_servicenode $CNT
+   init_h5_root_domain_local 
+fi
+
+
