6e8fd572b17abaf710214e64eab9fa5f04bfda00
jreadey <jreadey@hdfgroup.org>
2017-10-17 21:04:47 -0700

    skip env key check for EC2

--- runall.sh
+++ runall.sh
@@ -15,8 +15,11 @@ fi
 
 [ -z ${BUCKET_NAME} ] && echo "Need to set BUCKET_NAME" && exit 1
 [ -z ${AWS_S3_GATEWAY} ] && echo "Need to set AWS_S3_GATEWAY" && exit 1
-[ -z ${AWS_ACCESS_KEY_ID} ] && echo "Need to set AWS_ACCESS_KEY_ID" && exit 1
-[ -z ${AWS_SECRET_ACCESS_KEY} ] && echo "Need to set AWS_SECRET_ACCESS_KEY" && exit 1
+if [ -z $s3 ]; then
+  # if not using s3 and EC2 IAM roles, need to define AWS access keys
+  [ -z ${AWS_ACCESS_KEY_ID} ] && echo "Need to set AWS_ACCESS_KEY_ID" && exit 1
+  [ -z ${AWS_SECRET_ACCESS_KEY} ] && echo "Need to set AWS_SECRET_ACCESS_KEY" && exit 1
+fi
 
 s3=0
 if [ $# -gt 1 ]; then 
