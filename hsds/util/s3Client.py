662dd240996673369b719e438935a767fafb4502
John Readey <jreadey@hdfgroup.org>
2019-09-06 10:54:59 -0700

    added exception handling and s3 stats to s3Client class

--- hsds/util/s3Client.py
+++ hsds/util/s3Client.py
@@ -1,4 +1,5 @@
 import asyncio
+from asyncio import CancelledError
 from  inspect import iscoroutinefunction
 import subprocess
 import datetime
@@ -106,6 +107,21 @@ class S3Client():
  
         app['s3'] = self._client  # save so same client can be returned in subsequent calls
 
+    def _s3_stats_increment(self, counter, inc=1):
+        """ Incremenet the indicated connter
+        """
+        if "s3_stats" not in self._app:
+             return # app hasn't set up s3stats
+        s3_stats = self._app['s3_stats']
+        if counter not in s3_stats:
+            log.error(f"unexpected counter for s3_stats: {counter}")
+            return
+        if inc < 1:
+            log.error(f"unexpected inc for s3_stats: {inc}")
+            return
+             
+        s3_stats[counter] += inc
+
     async def get_object(self, key, bucket=None, range=None):
         """ Return data for object at given key.
            If Range is set, return the given byte range.
@@ -135,10 +151,19 @@ class S3Client():
                 log.info(msg)
                 raise HTTPNotFound()
             else:
-                # TBD - do this in storutil.py 
-                # s3_stats_increment(app, "error_count")
+                self._s3_stats_increment("error_count")
                 log.error(f"got unexpected ClientError on s3 get {key}: {ce}")
                 raise HTTPInternalServerError()
+        except CancelledError as cle:
+            self._s3_stats_increment("error_count")
+            msg = f"CancelledError putting s3 obj {key}: {cle}"
+            log.error(msg)
+            raise HTTPInternalServerError()
+        except Exception as e:
+            self._s3_stats_increment("error_count")
+            msg = f"Unexpected Exception {type(e)} putting s3 obj {key}: {e}"
+            log.error(msg)
+            raise HTTPInternalServerError()
         return data
 
     async def put_object(self, key, data, bucket=None):
@@ -156,15 +181,22 @@ class S3Client():
             log.info(f"s3Client.put_object({key} bucket={bucket}) start={start_time:.4f} finish={finish_time:.4f} elapsed={finish_time-start_time:.4f} bytes={len(data)}")
             s3_rsp = {"etag": rsp["ETag"], "size": len(data), "lastModified": int(finish_time)}
         except ClientError as ce:
-            # TBD - increment error_coun t in storUtil.py
-            #s3_stats_increment(app, "error_count")
+            self._s3_stats_increment("error_count")
             msg = f"Error putting s3 obj {key}: {ce}"
             log.error(msg)
             raise HTTPInternalServerError()
+        except CancelledError as cle:
+            #s3_stats_increment(app, "error_count")
+            msg = f"CancelledError putting s3 obj {key}: {cle}"
+            log.error(msg)
+            raise HTTPInternalServerError()
+        except Exception as e:
+            #s3_stats_increment(app, "error_count")
+            msg = f"Unexpected Exception {type(e)} putting s3 obj {key}: {e}"
+            log.error(msg)
+            raise HTTPInternalServerError()
         if data and len(data) > 0:
-            # TBD - do this in StorUtil.py
-            pass
-            #s3_stats_increment(app, "bytes_out", inc=len(data))
+            self._s3_stats_increment("bytes_out", inc=len(data))
         log.debug(f"s3Client.put_object {key} complete, s3_rsp: {s3_rsp}")  
         return s3_rsp
 
@@ -183,9 +215,18 @@ class S3Client():
                 log.warn(f"delete on s3key {key} but not found")
                 raise HTTPNotFound()
             # else some other error
-            # TBD - inc stat in storUtil.py
-            #s3_stats_increment(app, "error_count")
+            self._s3_stats_increment("error_count")
             msg = f"Error deleting s3 obj: {ce}"
+            log.error(msg)
+            raise HTTPInternalServerError()
+        except CancelledError as cle:
+            self._s3_stats_increment("error_count")
+            msg = f"CancelledError putting s3 obj {key}: {cle}"
+            log.error(msg)
+            raise HTTPInternalServerError()
+        except Exception as e:
+            self._s3_stats_increment("error_count")
+            msg = f"Unexpected Exception {type(e)} putting s3 obj {key}: {e}"
             log.error(msg)
             raise HTTPInternalServerError()
 
@@ -245,7 +286,7 @@ class S3Client():
             async for page in paginator.paginate(
                 PaginationConfig={'PageSize': 1000}, Bucket=bucket,  Prefix=prefix, Delimiter=deliminator):
                 assert not asyncio.iscoroutine(page)
-                #log.info(f"got page: {page}")
+                log.debug(f"got page: {page}")
                 self._getPageItems(page, key_names, include_stats=include_stats)
                 count += len(key_names)
                 if callback:
@@ -266,3 +307,13 @@ class S3Client():
         log.info(f"getS3Keys done, got {len(key_names)} keys")
                        
         return key_names
+
+    async def releaseClient(self):
+        """ release the client collection to s3
+           (Used for cleanup on application exit)
+        """
+        log.info("release S3Client")
+        if 's3' in self._app:
+            client = self._app['s3']
+            await client.close()
+            del self._app['s3']
