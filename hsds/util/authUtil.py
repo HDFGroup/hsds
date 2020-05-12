3f81cf0e4babc0e75038a55a21ba17639d316c7d
jreadey <jreadey@hdfgroup.org>
2020-05-11 21:50:16 -0700

    add log for token decoding

--- hsds/util/authUtil.py
+++ hsds/util/authUtil.py
@@ -499,6 +499,7 @@ def getUserPasswordFromRequest(request):
             msg = "Malformed authorization header"
             log.warn(msg)
             raise HTTPBadRequest(reason=msg)
+        log.debug(f"decoded auth header: {token_decoded}")
         if token_decoded.index(b':') < 0:
             msg = "Malformed authorization header (No ':' character)"
             log.warn(msg)
