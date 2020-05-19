b31bdea22d50b194f010ba31af178af986f70313
jreadey <jreadey@hdfgroup.org>
2020-05-19 12:19:19 -0700

    updates for config file with kubernetes

---
+++ admin/config/dump_config.py
@@ -0,0 +1,37 @@
+# this script can be used to validate the config and override yamls
+
+import yaml
+
+yml_file = "config.yml"
+try:
+    with open(yml_file, "r") as f:
+        yml_config = yaml.load(f)
+except FileNotFoundError as fnfe:
+    msg = f"Unable to find config file: {yml_file} - {fnfe}"
+    print(msg)
+    raise
+except yaml.scanner.ScannerError as se:
+    msg = f"Error parsing config.yml: {se}"
+    print(msg)
+    raise KeyError(msg)
+
+override_file = "override.yml"
+try:
+    with open(override_file, "r") as f:
+        override_config = yaml.load(f)
+except FileNotFoundError as fnfe:
+    msg = f"Unable to find override file: {override_file} - {fnfe}"
+    print(msg)
+    raise
+except yaml.scanner.ScannerError as se:
+    msg = f"Error parsing config.yml: {se}"
+    print(msg)
+    raise KeyError(msg)
+
+for k in yml_config:
+    v = yml_config[k]
+    if k in override_config:
+        o = override_config[k]
+        print(f"{k}: {o} (OVERRIDE of {v})")
+    else:
+        print(f"{k}: {v}")
--- k8s_make_configmap.sh
+++ k8s_make_configmap.sh
@@ -1,2 +1,2 @@
-create configmap hsds-config --from-file=admin/config/config.yml
-create configmap hsds-override --from-file=admin/config/override.yml
+kubectl create configmap hsds-config --from-file=admin/config/config.yml
+kubectl create configmap hsds-override --from-file=admin/config/override.yml
