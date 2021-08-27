bc5ae2757b931273d553ae102618544d0e98b637
Aleksandar Jelenak <ajelenak@hdfgroup.org>
2021-08-27 10:42:57 -0400

    Code clean up

--- testall.py
+++ testall.py
@@ -16,10 +16,13 @@ import sys
 PYTHON_CMD = "python3"
 
 unit_tests = ('arrayUtilTest', 'chunkUtilTest', 'domainUtilTest',
-    'dsetUtilTest', 'hdf5dtypeTest', 'idUtilTest', 'lruCacheTest', 'shuffleTest')
+              'dsetUtilTest', 'hdf5dtypeTest', 'idUtilTest', 'lruCacheTest',
+              'shuffleTest')
 
-integ_tests = ('uptest', 'setup_test', 'domain_test', 'group_test', 'link_test',
- 'attr_test', 'datatype_test', 'dataset_test', 'acl_test', 'value_test', 'pointsel_test', 'query_test', 'vlen_test')
+integ_tests = ('uptest', 'setup_test', 'domain_test', 'group_test',
+               'link_test', 'attr_test', 'datatype_test', 'dataset_test',
+               'acl_test', 'value_test', 'pointsel_test', 'query_test',
+               'vlen_test')
 
 skip_unit = False
 if len(sys.argv) > 1:
