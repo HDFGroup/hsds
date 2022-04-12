ea16852c0efa9f78ee6a3cd30d8c21481cfb536f
jreadey <jreadey@hdfgroup.org>
2022-04-11 17:30:17 -0700

    create chunk_list dataset with data initializer

--- tests/perf/write/create_empty.py
+++ tests/perf/write/create_empty.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 import h5pyd
 
 if len(sys.argv) == 1 or sys.argv[1] in ("-h", "--help"):
@@ -21,13 +22,20 @@ print("dset id:", dset.id.id)
 
 dt = [("start", "i8"), ("done", "i8"), ('status', "i4"), 
        ("x", "i4"), ("y", "i4"), ("nrow", "i4"), ("ncol", "i4"), ('pod', "S40")]
-table = f.create_table("chunk_list", dtype=dt)
+
+num_chunks = 0
+for _ in dset.iter_chunks():
+    num_chunks += 1
+print(f"{num_chunks} chunks in dataset dset")
+arr = np.zeros((num_chunks,), dtype=dt)  
+index = 0 
 for s in dset.iter_chunks():
     x = s[0].start
     y = s[1].start
     nrow = s[0].stop - s[0].start
     ncol = s[1].stop - s[1].start
-    entry = (0, 0, 0, x, y, nrow, ncol, "")
-    print('append:', entry)
-    table.append([entry,])
-    
+    arr[index] = (0,0,0,x,y,nrow,ncol,"")
+    index += 1
+f.create_dataset("chunk_list", data=arr)
+print("created chunk_list dataset")
+ 
