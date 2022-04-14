e59a39023677296516d7589b0c92291ded474ce8
jreadey <jreadey@hdfgroup.org>
2022-04-14 15:19:15 -0700

    added docker-compose for write test

---
+++ tests/perf/write/start.sh
@@ -0,0 +1,30 @@
+export HS_ENDPOINT=http://host.docker.internal:5101
+if [[ -z $HS_USERNAME ]] ; then
+  echo "HS_USERNAME not set" 
+  exit 1
+fi
+if [[ -z $HS_PASSWORD ]] ; then
+  echo "HS_PASWORD not set" 
+  exit 1
+fi
+if [[ -z $HS_WRITE_TEST_DOMAIN ]] ; then
+  echo "HS_WRITE_TEST_DOMAIN not set" 
+  exit 1
+fi
+if [[ -z $HS_BUCKET ]]; then
+  export HS_BUCKET=
+fi
+
+if [[ $# -gt 0 ]] ; then
+  if [[ $1 == "--help" ]] ; then
+    echo "./run.sh [count]" 
+    exit 0
+  fi
+  count=$1
+else
+  count=1
+fi
+
+echo "count $count"
+
+docker-compose -f docker-compose.yml up -d --scale hswritetest=$count
