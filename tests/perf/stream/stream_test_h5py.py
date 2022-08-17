2eb0ee6bec31c435274456297847c40608cc0b49
jreadey <jreadey@hdfgroup.org>
2022-08-16 17:34:15 -0700

    updates for stream_test

--- tests/perf/stream/stream_test_h5py.py
+++ tests/perf/stream/stream_test_h5py.py
@@ -5,66 +5,124 @@
 # This file is part of HSDS (HDF5 Scalable Data Service), Libraries and      #
 # Utilities.  The full HSDS copyright notice, including                      #
 # terms governing use, modification, and redistribution, is contained in     #
-# the file COPYING, which can be found at the root of the source code        #
+# the file COPYING, which can be fouERRORnd at the root of the source code        #
 # distribution tree.  If you do not have access to this file, you may        #
 # request a copy from help@hdfgroup.org.                                     #
 ##############################################################################
 from __future__ import print_function
 import math
 import numpy as np
+import logging
 import time
+import sys
 
-# set to 0 to use h5pyd/HSDS
-USE_H5PY = 1
-
-NCOLS = 12000
-NROWS = 2200
+ncols = 12000
+nrows = 2200
+use_h5py = False
+filepath = None
+compression = "gzip"
+loglevel = "warn"
 CHUNKS = (1500, 275)
 
-if USE_H5PY:
+
+
+if len(sys.argv) == 1 or sys.argv[1] in ("-h", "--help"):
+    s = "usage: python stream_test_h5pyd.py "
+    s += "[--ncols=n] "
+    s += "[--nrows=m] "
+    s += "[--comp=none|gzip]"
+    s += "[--loglevel=debug|info|warn|error]"
+    print(s)
+    print("for filepath, use hdf5:// prefix for h5pyd, posix path for h5py")
+    print("defaults...")
+    print(f"ncols={ncols}")
+    print(f"nrows={nrows}")
+    print(f"comp={compression}")
+    print(f"loglevel={loglevel}")
+    print("")
+    print("example: python stream_test_h5py.py hdf5://home/test_user1/bigfile.h5")
+    sys.exit(0)
+
+
+for arg in sys.argv:
+    print(arg)
+    if arg == sys.argv[0]:
+        continue
+    if arg.startswith("--ncols="):
+        arglen = len("--ncols=")
+        ncols = int(arg[arglen:])
+    elif arg.startswith("--nrows="):
+        arglen = len("--nrows=")
+        nrows = int(arg[arglen:])
+    elif arg.startswith("--comp="):
+        arglen = len("--comp=")
+        if len(arg) == arglen:
+            compression = None  # no compresion
+        else:
+            compression = arg[arglen:]
+    elif arg.startswith("--loglevel"):
+        arglen = len("--loglevel=")
+        loglevel = arg[arglen:]
+    elif arg.startswith("--"):
+        sys.exit(f"unexpected option: {arg}")
+    else:
+        filepath = arg
+
+if loglevel == "debug":
+    level = logging.DEBUG
+elif loglevel == "info":
+    level = logging.INFO
+elif loglevel == "warn":
+    level = logging.WARNING
+elif loglevel == "error":
+    level = logging.ERROR
+else:
+    sys.exit(f"unexpected loglevel: {loglevel}")
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=level)
+
+
+if filepath.startswith("hdf5://"):
+    import h5pyd as h5py
+else:
     import h5py
 
-    FILE_NAME = "bigfile.h5"
-else:
-    import h5pyd as h5py
-
-    FILE_NAME = "hdf5://home/h5user/stream/bigfile.h5"
 
 dt = np.dtype("u8")
 
-print("opening:", FILE_NAME)
+print(f"opening: {filepath}")
 
-f = h5py.File(FILE_NAME, "a")
+f = h5py.File(filepath, "a")
 
 if "dset2d" in f:
     # delete if dims have been updated
     dset2d = f["dset2d"]
-    if dset2d.shape[0] != NROWS or dset2d.shape[1] != NCOLS:
+    if dset2d.shape[0] != nrows or dset2d.shape[1] != ncols:
         del f["dset2d"]
 
 if "dset2d" not in f:
     # create dataset
-    f.create_dataset("dset2d", (NROWS, NCOLS), dtype=dt, chunks=CHUNKS)
+    f.create_dataset("dset2d", (nrows, ncols), dtype=dt, chunks=CHUNKS, compression=compression)
 
 dset2d = f["dset2d"]
 
 # initialize numpy array to test values
 print("initialzing data")
 
-arr = np.zeros((NROWS, NCOLS), dtype=dt)
-exp = int(math.log10(NCOLS)) + 1
-for i in range(NROWS):
+arr = np.zeros((nrows, ncols), dtype=dt)
+exp = int(math.log10(ncols)) + 1
+for i in range(nrows):
     row_start_value = i * 10 ** exp
-    for j in range(NCOLS):
+    for j in range(ncols):
         arr[i, j] = row_start_value + j + 1
 
 print("writing...")
-num_bytes = NROWS * NCOLS * dt.itemsize
+num_bytes = nrows * ncols * dt.itemsize
 ts = time.time()
 dset2d[:, :] = arr[:, :]
 elapsed = time.time() - ts
 mb_per_sec = num_bytes / (1024 * 1024 * elapsed)
-print(f" elapsed: {elapsed:.2f} s, {mb_per_sec:.2f} mb/s")
+print(f" elapsed: {elapsed:.2f} s, {mb_per_sec:.2f} MB/s")
 
 # read back the data as binary
 print("reading...")
@@ -73,7 +131,7 @@ arr_copy = dset2d[:, :]
 
 elapsed = time.time() - ts
 mb_per_sec = num_bytes / (1024 * 1024 * elapsed)
-print(f" elapsed: {elapsed:.2f} s, {mb_per_sec:.2f} mb/s")
+print(f" elapsed: {elapsed:.2f} s, {mb_per_sec:.2f} MB/s")
 
 if not np.array_equal(arr, arr_copy):
     print("arrays don't match!")
