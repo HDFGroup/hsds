c5f1777a11f5ca4f4a1900ac82701f5c25cfdec4
jreadey <jreadey@hdfgroup.org>
2023-03-01 08:48:41 +0100

    bump version number

--- hsds/basenode.py
+++ hsds/basenode.py
@@ -33,7 +33,7 @@ from .util.authUtil import isAdminUser
 from .util.k8sClient import getDnLabelSelector, getPodIps
 from . import hsds_logger as log
 
-HSDS_VERSION = "0.7.1"
+HSDS_VERSION = "0.7.2"
 
 
 def getVersion():
